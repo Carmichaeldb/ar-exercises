4e079b6bb67b8951b6fa5651cac6e208fc1f420b
David Carmichael <carmichael.db88@gmail.com>
2024-03-06 21:56:22 +0000

    Exercise 7: Completed

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :men_or_women_true
+
+  def men_or_women_true
+    if !mens_apparel && !womens_apparel
+      errors.add(:base, "mens_apperal or womans_apperal must be true")
+    end
+  end
 end
